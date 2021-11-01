e6d949f9bed5a982a6d3eb301a7d4a39fdd3e42e
nurzhanm2160 <ifk-100231@stud.umk.pl>
2021-11-01 01:22:45 +0100

    second commit: saving todos in localStroge, getting todos in localStorgae, fixing displayTodos where array has null

--- js/script.js
+++ js/script.js
@@ -3,11 +3,9 @@ let input = document.getElementById("todo");
 let actions = document.getElementById("action");
 let todos = [];
 
-
-
 const displayTodos = () => {
     let displayTodo = ''
-
+    
     todos.forEach(function(item, i){
         displayTodo += `<tr id="item_${i}">
             <td>
@@ -16,25 +14,36 @@ const displayTodos = () => {
                 <span></span>
             </td>
             </label>
-            <td>${item.action}</td>
+            <td id="text_${i}">${item.action}</td>
             <td>${item.date}</td>
-            <th>
-                <a class="waves-effect red accent-4 btn-small" id="delete_${i}">
-                    <img src="img/x.png">
-                </a>
-            </th>
             <th>
                 <a class="waves-effect waves-teal btn-flat" id="edit_${i}">
                     Edit
+                </a>
+            </th>
+            <th>
+                <a class="waves-effect red accent-4 btn-small" id="delete_${i}">
+                    X
                 </a>
             </th>
         </tr>`
             actions.innerHTML = displayTodo;
     })   
     
-    if(todos[0] == undefined) {
+    if(todos[0] == null) {
         actions.innerHTML = displayTodo;
     }
+}
+
+
+if(localStorage.getItem('todo')) {
+    todos = JSON.parse(localStorage.getItem('todo'));
+    todos.filter((element, i) => {
+        if(element == null) {
+            delete todos[i]
+        }
+    })
+    displayTodos();
 }
 
 button.addEventListener('click', function() {
@@ -49,15 +58,34 @@ button.addEventListener('click', function() {
 
     todos.push(newTodo);
     displayTodos();
+    localStorage.setItem('todo', JSON.stringify(todos));
 });
 
+const editToDo = (todoId) => {
+    let editable = todos[todoId];
+    let message = ''
+    let editSelector = document.getElementById(`item_${todoId}`);
+    message = `<h1>Проверка редактирования<h1>`;
+    editSelector.innerHTML += message;
+    
+}
+
 actions.addEventListener('click', function(e) {
-    todoId = e.target.id.slice(1,2);
-    console.log(todoId)
-    delete todos[todoId];
-    console.log(todos)
+    todoId = e.target.id.replace(/\D+/g,"");
+    if(e.target.id.slice(1,2) === 'e') {
+        delete todos[todoId]
+        localStorage.setItem('todo', JSON.stringify(todos));
+    } else if (e.target.id.slice(1,2) === 'd') {
+        editToDo(todoId);
+    }
     displayTodos();
 })
+
+// TODO: complete todo editing 
+// actions.addEventListener('dblclick', function(e){
+//     todoId = e.target.id.replace(/\D+/g,"");
+    
+// })
 
 // actions.addEventListener('click', function(e) {
 //     editId = e.target.id.slice(5,6);
